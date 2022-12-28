33ae0f9be1247113011a505c07a6d6c066a058dd
Oren Mittman <nihil2501@gmail.com>
2022-12-27 22:56:02 -0500

    code exchange refactor wip

--- src/code_exchange.rs
+++ src/code_exchange.rs
@@ -1,20 +1,32 @@
 use std::result;
-use wasmbus_rpc::actor::prelude::*;
-use wasmcloud_interface_keyvalue::{
-    GetResponse, IncrementRequest, KeyValue, KeyValueSender, SetRequest,
-};
 
 #[async_trait]
-pub trait CodeExchange {
+trait CodeExchange {
     async fn create(&self, message: String) -> CreateResult;
     async fn find(&self, code: String) -> FindResult;
+    // TODO: reconsider
+    async fn generate_code(&self) -> GenerateCodeResult;
+}
+
+type CreateResult = result::Result<String, CreateError>;
+enum CreateError {
+    OverCapacity,
+    Unknown(RpcError),
+}
+
+type FindResult = result::Result<String, FindError>;
+enum FindError {
+    NotFound,
+    Unknown(RpcError),
 }
 
 // Codes last for a day.
 const CODE_EXPIRY: u32 = 86_400;
+const CODE_LIST: &[&str] = &["hello", "goodbye"];
+const CODE_LIST_INDEX_KEY: &str = "code_list_index";
 
 #[async_trait]
-impl CodeExchange for ActorCodeExchange<'_> {
+impl<T: KeyValueStore> CodeExchange for T {
     async fn create(&self, message: String) -> CreateResult {
         let code = self.generate_code().await?;
         self.set(&code, &message, CODE_EXPIRY).await?;
@@ -23,18 +35,12 @@ impl CodeExchange for ActorCodeExchange<'_> {
 
     async fn find(&self, code: String) -> FindResult {
         let response = self.get(&code).await?;
-        if response.exists {
-            Ok(response.value)
-        } else {
-            Err(FindError::NotFound)
+        match response {
+            Some(message) => Ok(message),
+            None => Err(FindError::NotFound),
         }
     }
-}
 
-const CODE_LIST: &[&str] = &["hello", "goodbye"];
-const CODE_LIST_INDEX_KEY: &str = "code_list_index";
-
-impl ActorCodeExchange<'_> {
     async fn generate_code(&self) -> GenerateCodeResult {
         // Depends on atomic increment. Our relaxed strategy (one we still need
         // to prove out that it is suitable enough) is as follows:
@@ -44,28 +50,86 @@ impl ActorCodeExchange<'_> {
         let index = (index as usize) % CODE_LIST.len();
 
         // 2. From index, fetch candidate codeword and check if it exists.
-        // Expiry handled by kv-store
+        // Expiry handled by kv-store.
         let code = CODE_LIST[index];
         let response = self.get(code).await?;
-        if response.exists {
-            // If yes, error out with a `OverCapacity` and also decrement back.
-            self.incr_by(CODE_LIST_INDEX_KEY, -1).await?;
-            Err(GenerateCodeError::OverCapacity)
-        } else {
-            // Otherwise use it.
-            Ok(code.to_string())
+        match response {
+            // If not, use it.
+            None => Ok(code.to_string()),
+            // Otherwise, error out with a `OverCapacity` and also decrement
+            // back.
+            Some(_) => {
+                self.incr_by(CODE_LIST_INDEX_KEY, -1).await?;
+                Err(GenerateCodeError::OverCapacity)
+            }
         }
     }
 }
 
-// Just some helpers so things are easier to look at.
-impl ActorCodeExchange<'_> {
-    pub fn new(ctx: &Context) -> ActorCodeExchange {
-        ActorCodeExchange { ctx }
-    }
+type GenerateCodeResult = result::Result<String, GenerateCodeError>;
+enum GenerateCodeError {
+    OverCapacity,
+    Unknown(RpcError),
+}
 
-    async fn get(&self, code: &str) -> RpcResult<GetResponse> {
-        KeyValueSender::new().get(self.ctx, code).await
+impl From<RpcError> for CreateError {
+    fn from(error: RpcError) -> Self {
+        CreateError::Unknown(error)
+    }
+}
+
+impl From<GenerateCodeError> for CreateError {
+    fn from(error: GenerateCodeError) -> Self {
+        match error {
+            GenerateCodeError::OverCapacity => CreateError::OverCapacity,
+            GenerateCodeError::Unknown(error) => CreateError::Unknown(error),
+        }
+    }
+}
+
+impl From<RpcError> for FindError {
+    fn from(error: RpcError) -> Self {
+        FindError::Unknown(error)
+    }
+}
+
+impl From<RpcError> for GenerateCodeError {
+    fn from(error: RpcError) -> Self {
+        GenerateCodeError::Unknown(error)
+    }
+}
+
+#[async_trait]
+trait KeyValueStore {
+    async fn get(&self, key: &str) -> RpcResult<Option<String>>;
+    async fn set(&self, key: &str, value: &str, expires: u32) -> RpcResult<()>;
+    async fn incr_by(&self, key: &str, value: i32) -> RpcResult<i32>;
+}
+
+use wasmbus_rpc::actor::prelude::*;
+use wasmcloud_interface_keyvalue::{
+    IncrementRequest, KeyValue, KeyValueSender, SetRequest,
+};
+
+struct KeyValueStoreActor<'a> {
+    ctx: &'a Context,
+}
+
+impl KeyValueStoreActor<'_> {
+    pub fn new(ctx: &Context) -> KeyValueStoreActor {
+        KeyValueStoreActor { ctx }
+    }
+}
+
+#[async_trait]
+impl KeyValueStore for KeyValueStoreActor<'_> {
+    async fn get(&self, code: &str) -> RpcResult<Option<String>> {
+        let response = KeyValueSender::new().get(self.ctx, code).await?;
+        if response.exists {
+            Ok(Some(response.value))
+        } else {
+            Ok(None)
+        }
     }
 
     async fn set(&self, key: &str, value: &str, expires: u32) -> RpcResult<()> {
@@ -91,57 +155,5 @@ impl ActorCodeExchange<'_> {
                 },
             )
             .await
-    }
-}
-
-pub type CreateResult = result::Result<String, CreateError>;
-
-pub enum CreateError {
-    OverCapacity,
-    Unknown(RpcError),
-}
-
-impl From<RpcError> for CreateError {
-    fn from(error: RpcError) -> Self {
-        CreateError::Unknown(error)
-    }
-}
-
-impl From<GenerateCodeError> for CreateError {
-    fn from(error: GenerateCodeError) -> Self {
-        match error {
-            GenerateCodeError::OverCapacity => CreateError::OverCapacity,
-            GenerateCodeError::Unknown(error) => CreateError::Unknown(error),
-        }
-    }
-}
-
-pub type FindResult = result::Result<String, FindError>;
-
-pub enum FindError {
-    NotFound,
-    Unknown(RpcError),
-}
-
-impl From<RpcError> for FindError {
-    fn from(error: RpcError) -> Self {
-        FindError::Unknown(error)
-    }
-}
-
-pub struct ActorCodeExchange<'a> {
-    ctx: &'a Context,
-}
-
-type GenerateCodeResult = result::Result<String, GenerateCodeError>;
-
-enum GenerateCodeError {
-    OverCapacity,
-    Unknown(RpcError),
-}
-
-impl From<RpcError> for GenerateCodeError {
-    fn from(error: RpcError) -> Self {
-        GenerateCodeError::Unknown(error)
     }
 }
